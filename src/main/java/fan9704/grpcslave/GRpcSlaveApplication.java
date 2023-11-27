ffb32f6bec5a534bad298ed52a6c77eb7ed74fc3
fan9704 <cxz123499@gmail.com>
2023-11-27 17:02:36 +0800

    Init Commit

---
+++ src/main/java/fan9704/grpcslave/GRpcSlaveApplication.java
@@ -0,0 +1,13 @@
+package fan9704.grpcslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcSlaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GRpcSlaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fan9704/grpcslave/GRpcSlaveApplicationTests.java
@@ -0,0 +1,13 @@
+package fan9704.grpcslave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcSlaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
